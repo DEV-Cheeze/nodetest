28de53fb665396d28a9557b3e4f98394a0f434bf
Dev-Cheeze <ckdwnd9726@naver.com>
2023-03-29 17:52:07 +0900

    test2

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("Hello World!");
+const id = document.querySelector("#id"), // 질의 선택자
+psword = document.querySelector("#psword"),
+loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        password : psword.value
+    };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    }) 
+}
